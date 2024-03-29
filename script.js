86cc283282bef6596b532ed074067a6bbf191546
Felipedgws <felipe.augusto1202@gmail.com>
2024-03-29 01:14:15 -0300

    a
    a

---
+++ script.js
@@ -0,0 +1,39 @@
+function mostrar1(){
+    var text1 = document.getElementById("conteudo1")
+
+    if(text1.style.display === "none"){
+        text1.style.display = "block"
+    }else{
+        text1.style.display = "none"
+    }
+}
+
+function mostrar2(){
+    var text2 = document.getElementById("conteudo2")
+
+    if(text2.style.display === "none"){
+        text2.style.display = "block"
+    }else{
+        text2.style.display = "none"
+    }
+}
+
+function mostrar3(){
+    var text3 = document.getElementById("conteudo3")
+
+    if(text3.style.display === "none"){
+        text3.style.display = "block"
+    }else{
+        text3.style.display = "none"
+    }
+}
+
+function mostrar4(){
+    var text4 = document.getElementById("conteudo4")
+
+    if(text4.style.display === "none"){
+        text4.style.display = "block"
+    }else{
+        text4.style.display = "none"
+    }
+}
